266bd3a4126554b40fb1ddd781e45631899917ed
Alberto Marcos <130506688+betomarcos@users.noreply.github.com>
2024-06-28 13:29:15 -0500

    Update garmin_activities_stage.sql

--- garmin_activities_stage.sql
+++ garmin_activities_stage.sql
@@ -1,31 +1,42 @@
-drop table garmin_activities_stage;
+DROP TABLE IF EXISTS garmin_activities_stage;
+
 CREATE TABLE garmin_activities_stage AS
 SELECT
     CASE 
+        WHEN activity_type LIKE '%Multisport%' THEN 'Triathlon'
         WHEN activity_type LIKE '%Cycling%' OR activity_type LIKE '%Biking%' THEN 'Cycling' 
         WHEN activity_type LIKE '%Running%' THEN 'Running' 
         WHEN activity_type LIKE '%Walking%' THEN 'Walking' 
         WHEN activity_type LIKE '%Yoga%' THEN 'Yoga' 
         WHEN activity_type LIKE '%Strength Training%' THEN 'Strength Training' 
-        ELSE 'Other'  -- will be able to filter out the "other" fields in Tableau.
+        ELSE 'Other' -- Other includes: swim, golf, soccer, cardio, breathwork, etc
     END AS activity_type,
     DATE(activity_date) AS activity_date,
     Title AS title,
-    CAST(avg_hr AS DECIMAL(10, 2)) AS avg_hr,
-    CAST(max_hr AS DECIMAL(10, 2)) AS max_hr,
-    CAST(aerobic_te AS DECIMAL(5, 1)) AS aerobic_te,
-    CAST(distance_km AS DECIMAL(10, 2)) AS distance_km,
+    avg_hr,
+    max_hr,
+    aerobic_te,
+    distance_km,
     Calories AS calories,
     activity_time,
-    ROUND(TIME_TO_SEC(activity_time) / 60) AS duration_minutes
+    ROUND(
+        CASE
+            WHEN activity_time LIKE '%.%' THEN
+                -- Minutes:Seconds format
+                SUBSTRING_INDEX(activity_time, ':', 1) * 60 +
+                SUBSTRING_INDEX(activity_time, ':', -1)
+            ELSE
+                -- Hours:Minutes:Seconds format
+                SUBSTRING_INDEX(activity_time, ':', 1) * 3600 +
+                SUBSTRING_INDEX(SUBSTRING_INDEX(activity_time, ':', -2), ':', 1) * 60 +
+                SUBSTRING_INDEX(activity_time, ':', -1)
+        END / 60
+    ) AS duration_minutes -- fixes issue with having some 'activity_type' fields measured in minutes vs hours. Example: Yoga activities were in mm:ss. Running activities were in hh:mm:ss.
 FROM garmin_all_activities_raw
 WHERE 
-    avg_hr IS NOT NULL AND TRIM(avg_hr) != '' AND CAST(avg_hr AS DECIMAL(10, 2)) > 0
+    (avg_hr IS NOT NULL AND TRIM(avg_hr) != '' AND avg_hr > 0)
+    OR (activity_type LIKE '%Multisport%') -- Multisport types have 0 as AVG HR. But still want to bring those to the visuals.
     AND distance_km IS NOT NULL AND TRIM(distance_km) != ''
-    AND max_hr IS NOT NULL AND TRIM(max_hr) != '' AND CAST(max_hr AS DECIMAL(10, 2)) > 0
-    AND aerobic_te IS NOT NULL AND TRIM(aerobic_te) != '' AND CAST(aerobic_te AS DECIMAL(5, 1)) >= 0
-    AND activity_date IS NOT NULL
-    AND activity_time IS NOT NULL
 HAVING 
-    max_hr < 201 -- this may change later on
+    max_hr < 201 -- this could change later on, currently limiting to under 201 bpm due to a Garmin HR monitor strap issue, some activities have HR of 240-250, which is not accurate as the activity had not even started.
 ;
